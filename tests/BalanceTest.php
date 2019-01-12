ddffbcae8a86eb3bb8a99afbf472b30da3d70602
Igor Fedoronchuk <fedoronchuk@gmail.com>
2019-01-12 02:18:09 +0200

    improve tests for Balance Item

--- tests/BalanceTest.php
+++ tests/BalanceTest.php
@@ -8,13 +8,18 @@ class BalanceTest extends BaseTest
     {
         $this->startVCR('balance.yml');
 
-        $balance = \Didww\Item\Balance::find();
-        $this->assertInstanceOf('Didww\Item\Balance', $balance->getData());
-        $this->assertEquals($balance->getData()->getAttributes(), [
+        $balanceDocument = \Didww\Item\Balance::find();
+        $balance = $balanceDocument->getData();
+        $this->assertInstanceOf('Didww\Item\Balance', $balance);
+        $this->assertEquals($balance->getAttributes(), [
             'balance' => '50.00',
             'credit' => '10.00',
             'total_balance' => '60.00',
         ]);
+
+        $this->assertEquals($balance->getTotalBalance(), 60.00);
+        $this->assertEquals($balance->getCredit(), 10.00);
+        $this->assertEquals($balance->getBalance(), 50.00);
 
         $this->stopVCR();
     }
