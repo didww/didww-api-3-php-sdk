79d2d9f22041e4aa3b13dd2bf9b32824ebdcda17
Denis Talakevich <senid231@gmail.com>
2021-04-09 14:00:12 +0300

    improve encryption
    add tests

--- tests/EncryptTest.php
+++ tests/EncryptTest.php
@@ -4,6 +4,21 @@ namespace Didww\Tests;
 
 class EncryptTest extends BaseTest
 {
+    private function decrypt(string $binary, string $privateKey, int $keyIndex): string
+    {
+        $encryptedAesCredentials = 0 == $keyIndex ? substr($binary, 0, 512) : substr($binary, 512, 512);
+        $encryptedAesData = substr($binary, 1024);
+        $rsa = \phpseclib3\Crypt\RSA::load($privateKey)
+            ->withHash('sha256')
+            ->withMGFHash('sha256');
+
+        $aesCredentials = $rsa->decrypt($encryptedAesCredentials);
+        $aesKey = substr($aesCredentials, 0, 32);
+        $aesIV = substr($aesCredentials, 32); // 16 bytes
+
+        return openssl_decrypt($encryptedAesData, 'aes-256-cbc', $aesKey, OPENSSL_RAW_DATA, $aesIV);
+    }
+
     public function testAll()
     {
         $this->startVCR('public_keys.yml');
@@ -11,7 +26,7 @@ class EncryptTest extends BaseTest
 
         $fingerprint = $encrypt->getFingerprint();
         $this->assertEquals(
-            'c74684d7863639169c21c4d04747f8d6fa05cfe3:::8a586bd37fa0000501715321b2e6a7b3ca57894c',
+            'ca5af2d14bee923a0a0d1687b7c77e7211a57f84:::683150ee69b4d906aa883d0ac12b0fdd79f95bcf',
             $fingerprint
         );
 
@@ -24,8 +39,16 @@ class EncryptTest extends BaseTest
 
         $this->assertEquals($keys, $encrypt->getPublicKeys());
 
-        $encryptedData = $encrypt->encrypt('test');
-        $this->assertStringContainsString(':::', $encryptedData);
+        $data = 'test content';
+        $encryptedData = $encrypt->encrypt($data);
+
+        $privateKeys = json_decode(file_get_contents('tests/fixtures/private_keys.json'), true);
+        $decryptedA = $this->decrypt($encryptedData, $privateKeys['private_key_a'], 0);
+        $this->assertEquals($data, $decryptedA);
+
+        $decryptedB = $this->decrypt($encryptedData, $privateKeys['private_key_b'], 1);
+        $this->assertEquals($data, $decryptedB);
+
         $this->stopVCR();
     }
 }
