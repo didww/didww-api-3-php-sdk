de23eef91ef26553fba2fbff96fcf7c5163e771d
Denis Talakevich <senid231@gmail.com>
2022-02-02 12:40:35 +0200

    rename Trunk and TrunkGroup to VoiceInTrunk and VoiceInTrunkGroup

---
+++ src/Item/VoiceInTrunkGroup.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Didww\Item;
+
+class VoiceInTrunkGroup extends BaseItem
+{
+    use \Didww\Traits\Fetchable;
+    use \Didww\Traits\Saveable;
+    use \Didww\Traits\Deletable;
+
+    protected $type = 'voice_in_trunk_groups';
+
+    public function voiceInTrunks()
+    {
+        return $this->hasMany(VoiceInTrunk::class);
+    }
+
+    public function setVoiceInTrunks(\Swis\JsonApi\Client\Collection $voiceInTrunks)
+    {
+        $this->voiceInTrunks()->associate($voiceInTrunks);
+    }
+
+    protected function getWhiteListAttributesKeys()
+    {
+        return [
+          'capacity_limit',
+          'name',
+       ];
+    }
+}
