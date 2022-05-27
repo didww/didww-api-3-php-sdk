9dd664fa8b3b3aa336fd71971a2a8846953296d3
Ivanov-Anton <jinsike123@gmail.com>
2022-05-27 13:22:42 +0300

    register NANPA prefixes resource for DIDWW PHP SDK

---
+++ tests/NanpaPrefixTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Didww\Tests;
+
+class NanpaPrefixTest extends BaseTest
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->startVCR('nanpa_prefixes.yml');
+    }
+
+    protected function tearDown()
+    {
+        $this->stopVCR();
+    }
+
+    public function testAll()
+    {
+        $nanpaPrefixDocument = \Didww\Item\NanpaPrefix::all();
+        $this->assertEquals('2', $nanpaPrefixDocument->getMeta()['total_records']);
+    }
+
+    public function testMetaData()
+    {
+        $nanpaPrefixDocument = \Didww\Item\NanpaPrefix::all();
+        $this->assertEquals('2022-05-09', $nanpaPrefixDocument->getMeta()['api_version']);
+    }
+
+    public function testCount()
+    {
+        $nanpaPrefixDocument = \Didww\Item\NanpaPrefix::all();
+        $this->assertCount(2, $nanpaPrefixDocument->getData());
+    }
+
+    public function testFind()
+    {
+        $uuid = '6c16d51d-d376-4395-91c4-012321317e48';
+        $nanpaPrefixDocument = \Didww\Item\NanpaPrefix::find($uuid, ['include' => 'country']);
+        $data = $nanpaPrefixDocument->getData();
+        $this->assertEquals(['npa' => '864', 'nxx' => '920'], $data->getAttributes());
+    }
+}
