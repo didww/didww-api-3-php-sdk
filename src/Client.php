d6f8a52671ac6e7afd20383a0a259f29adecb00c
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-03-10 23:03:52 +0100

    Skip Api-Key header on public_keys endpoint

--- src/Client.php
+++ src/Client.php
@@ -4,6 +4,19 @@ namespace Didww;
 
 class Client extends \Swis\JsonApi\Client\Client
 {
+    private const PUBLIC_KEYS_ENDPOINT = 'public_keys';
+
+    protected function buildRequest(string $method, string $endpoint, $body = null, array $headers = []): \Psr\Http\Message\RequestInterface
+    {
+        $request = parent::buildRequest($method, $endpoint, $body, $headers);
+
+        if (str_contains($endpoint, self::PUBLIC_KEYS_ENDPOINT)) {
+            $request = $request->withoutHeader('api-key');
+        }
+
+        return $request;
+    }
+
     public static function sdkVersion(): string
     {
         if (class_exists(\Composer\InstalledVersions::class)) {
--- tests/PublicKeyTest.php
+++ tests/PublicKeyTest.php
@@ -17,4 +17,32 @@ class PublicKeyTest extends BaseTest
         $this->assertCount(2, $publicKeys);
         $this->stopVCR();
     }
+
+    public function testPublicKeysRequestOmitsApiKeyHeader()
+    {
+        $documentClient = \Didww\Configuration::getDocumentClient();
+        $reflection = new \ReflectionProperty($documentClient, 'client');
+        $reflection->setAccessible(true);
+        $client = $reflection->getValue($documentClient);
+
+        $buildRequest = new \ReflectionMethod($client, 'buildRequest');
+        $buildRequest->setAccessible(true);
+
+        $request = $buildRequest->invoke($client, 'GET', 'public_keys');
+        $this->assertFalse($request->hasHeader('api-key'), 'public_keys request should not include api-key header');
+    }
+
+    public function testOtherRequestsIncludeApiKeyHeader()
+    {
+        $documentClient = \Didww\Configuration::getDocumentClient();
+        $reflection = new \ReflectionProperty($documentClient, 'client');
+        $reflection->setAccessible(true);
+        $client = $reflection->getValue($documentClient);
+
+        $buildRequest = new \ReflectionMethod($client, 'buildRequest');
+        $buildRequest->setAccessible(true);
+
+        $request = $buildRequest->invoke($client, 'GET', 'dids');
+        $this->assertTrue($request->hasHeader('api-key'), 'non-public_keys request should include api-key header');
+    }
 }
