17a45bc945a7c9a322ab337ec6ad53985b9caffb
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-17 10:16:42 +0200

    feat: add EmergencyRequirement resource (/v3/emergency_requirements) for 2026-04-16
    Read-only resource describing requirements for ordering emergency
    calling services. Fields: identity_type, address/personal/business
    area_level and mandatory_fields, estimate_setup_time,
    requirement_restriction_message. Relations: country, did_group_type.

--- src/TypeMapper.php
+++ src/TypeMapper.php
@@ -40,5 +40,6 @@ class TypeMapper extends \Swis\JsonApi\Client\TypeMapper
         $this->setMapping('voice_out_trunks', Item\VoiceOutTrunk::class);
         $this->setMapping('voice_out_trunk_regenerate_credentials', Item\VoiceOutTrunkRegenerateCredential::class);
         $this->setMapping('did_history', Item\DidHistory::class);
+        $this->setMapping('emergency_requirements', Item\EmergencyRequirement::class);
     }
 }
